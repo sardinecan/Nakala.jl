77fe8059263e19acf16d23975d01d762714b3e2c
sardinecan <morvan.josselin@gmail.com>
2024-09-09 18:59:22 +0200

    added utilities functions

--- src/utilities.jl
+++ src/utilities.jl
@@ -14,19 +14,270 @@ end
 export contains_key_value
 
 
+"""
+    getdatas_resume(datas::Vector{Any})
 
+À partir d'un tableau de données issu de Nakala, retourne un autre tableau composé de paires `titre - identifiant de la donnée`.
+"""
 function getdatas_resume(datas::Vector{Any})
   list = Vector()
   for data in datas
-    identifier = get(data, "identifier", "")
-    metas = get(data, "metas", "")
-    title = filter(x -> get(x, "propertyUri", "") == "http://nakala.fr/terms#title", metas)[1]
-    item = Dict(get(title, "value", "noTitle") => identifier)
+    identifier = data["identifier"]
+    metas = data["metas"]
+    title = filter(x -> x["propertyUri"] == "http://nakala.fr/terms#title", metas)[1]
+    item = get(title, "value", "noTitle") => identifier
     push!(list, item)
   end
 
   return list
 end
 export getdatas_resume
+
+
+"""
+    getfiles_urls_from_data(data::Dict, filenames::Vector,  apiTest::Bool=false)
+À partir d'une liste de fichiers dans une donnée, retourne les urls de téléchargement.
+"""
+function getfiles_urls_from_data(data::Dict, filenames::Vector, apiTest::Bool=false)
+  apiTest==false ? apiurl = "https://api.nakala.fr" : apiurl = "https://apitest.nakala.fr"
+  identifier = data["identifier"]
+  filesList = data["files"]
+
+  urls = Vector()
+  for file in filenames
+      item = filter(x -> x["name"] == file, filesList)[1]
+      fileIdentifier = item["sha1"]
+
+      fileUrl = joinpath(apiurl, "data", identifier, fileIdentifier)
+      push!(urls, fileUrl)
+  end
+
+  return urls
+end
+export getfiles_urls_from_data
+
+"""
+
+liste les fichiers contenus dans un dossier (`path`) et écrit cette liste dans un `files.csv`. Si le dossier comporte des sous-dossier, leur contenu est listé de la même manière. 
+"""
+function listfiles(path)
+  directories = []
+  for (root, dirs, files) in walkdir(path)
+    filter!(x -> startswith(x, ".") == false, dirs)
+    for dir in dirs
+      touch(joinpath(root, dir, "files.csv"))
+      f = open(joinpath(root, dir, "files.csv"), "w")
+        write(f, "filename")
+      close(f)
+
+      println(dir)
+      push!(directories, dir)
+
+      list = readdir(joinpath(root, dir))
+
+      for file in list
+        f = open(joinpath(root, dir, "files.csv"), "a")
+          write(f, "\n"*file)
+        close(f)
+      end
+    end
+  end
+  
+  return directories
+end
+
+"""
+dépot de fichiers sur Nakala à partir d'un liste csv
+@arg directoryPath : chemin vers le dossier contenant les fichiers à déposer et la liste
+
+@return : array [ métadonnées des fichiers déposés ]
+"""
+function postfiles_from_list(path::String, apiTest::Bool=false)
+  files2upload = CSV.read(joinpath(path, "files.csv"), DataFrame, header=1) # fichier de métadonnées 
+
+  #%% Dépôt des fichiers
+  files = Vector()
+  filesInfo = []
+  
+  for (i, row) in enumerate(eachrow(files2upload))
+    filename = row[:filename]
+    
+    println("Envoi du fichier n°", i, " - ", filename)
+    fileResponse = postFile(joinpath(directoryPath, filename))
+    fileIdentifier = fileResponse["sha1"]
+
+    push!(files, fileResponse) # récupération de l'identifiant Nakala du fichier (fileIdentifier) pour le dépot des métadonnées et de la ressource
+    push!(filesInfo, [filename, fileIdentifier])
+  end
+  
+  return [files, filesInfo]
+end
+
+
+#=
+metadataFromCsv : établissement des métadonnées d'une donnée à partir d'un fichier csv
+@arg path : chemin vers le fichier de métadonnées
+=#
+function metadata_from_csv(path::String)
+  metadata = CSV.read(path, DataFrame, header=1) # fichier de métadonnées 
+
+  title = metadata[!, :title][1] 
+  metadata[!, :collections][1] !== missing  ? collections = split(metadata[!, :collections][1], ";") : collections = nothing
+  authors = split(metadata[!, :authors][1], ";")
+  date = metadata[!, :date][1]
+  license = metadata[!, :licence][1]
+  status = metadata[!, :status][1]
+  datatype = metadata[!, :datatype][1]
+  description = metadata[!, :description][1]
+  metadata[!, :collections][1] !== missing  ? keywords = split(metadata[!, :keywords][1], ";") : keywords = nothing
+  metadata[!, :collections][1] !== missing  ? datarights = split(metadata[!, :rights][1], ";") : datarights = nothing
+  lang = metadata[!, :lang][1]
+  
+  # métadonnées de la ressource
+  meta = Vector()
+
+  # titre (obligatoire)
+  metaTitle = Dict(
+    :value => title,
+    :typeUri => "http://www.w3.org/2001/XMLSchema#string",
+    :propertyUri => "http://nakala.fr/terms#title",
+    :lang => lang
+
+  )
+  push!(meta, metaTitle)
+
+  # datatype (obligatoire)
+  metaType = Dict(
+    :value => datatype,
+    :typeUri => "http://www.w3.org/2001/XMLSchema#anyURI",
+    :propertyUri => "http://nakala.fr/terms#type"
+  )
+  push!(meta, metaType)
+
+  # authorité/creator (obligatoire, mais accepte la valeur null)
+  for author in authors   
+    if length(split(author, ",")) > 1
+      identity = split(author, ",")
+      metaAuthor = Dict(
+        :value => Dict(
+          :givenname => identity[2],
+          :surname => identity[1]
+        ),
+        :propertyUri => "http://nakala.fr/terms#creator"
+      )
+      push!(meta, metaAuthor)
+    else
+      metaAuthor = Dict(
+        :value => Dict(
+          :givenname => author,
+          :surname => author
+        ),
+        :propertyUri => "http://nakala.fr/terms#creator"
+      )
+      push!(meta, metaAuthor)
+    end
+  end
+
+  # date (obligatoire, mais accepte la valeur null)    
+  metaCreated = Dict(
+    :value => Dates.today(),
+    :typeUri => "http://www.w3.org/2001/XMLSchema#string",
+    :propertyUri => "http://nakala.fr/terms#created"
+  )
+  push!(meta, metaCreated)
+    
+  # licence (obligatoire pour une donnée publiée)
+  metaLicense = Dict(
+    :value => license,
+    :typeUri => "http://www.w3.org/2001/XMLSchema#string",
+    :propertyUri => "http://nakala.fr/terms#license"
+  )
+  push!(meta, metaLicense)
+
+  # Droits (facultatif)
+  rights = []
+  if datarights !== nothing
+    for dataright in datarights
+      right = Dict(
+        :id => split(dataright, ",")[1],
+        :role => split(dataright, ",")[2]
+      )
+      push!(rights, right)
+    end
+  end
+
+  # Description (facultatif)
+  metaDescription = Dict(
+    :value => description,
+    :lang => lang,
+    :typeUri => "http://www.w3.org/2001/XMLSchema#string",
+    :propertyUri => "http://purl.org/dc/terms/description"
+  )
+  push!(meta, metaDescription)
+
+  # Mots-clés
+  if keywords !== nothing
+    for keyword in keywords
+      metaKeyword = Dict(
+        :value => keyword,
+        :lang => lang,
+        :typeUri => "http://www.w3.org/2001/XMLSchema#string",
+        :propertyUri => "http://purl.org/dc/terms/subject"
+      )
+      push!(meta, metaKeyword)
+    end
+  end
+
+  # assemblage des métadonnées avant envoi de la ressource
+  body = Dict{Symbol, Any}(
+    :collectionsIds => collections,
+    :status => "pending"
+    :metas => meta,
+    :rights => rights
+  )
+
+  return body
+end
+
+#=
+submitDataFromFolder : dépôt d'une donnée à partir d'un dossier contenant les fichiers constitutifs de la donnée et les métadonnées contenus dans un ficheir csv
+@arg path : chemin vers le dossier contenant les données à déposer
+@arg directory : nom du dossier constitutif de la donnée
+=#
+function postdatas_from_folder(path::String, directory::String)
+  postedFilesFromList = postFilesFromList(joinpath(path, directory))
+  files = Dict( :files => postedFilesFromList[1] )
+  filesInfo = postedFilesFromList[2]
+
+  metadata = metadataFromCsv(joinpath(path, directory, "metadata.csv"))
+
+  merge!(metadata, files) 
+
+  postedData = postData(metadata)
+  dataIdentifier = postedData["payload"]["id"] # récupération de l'identifiant Nakala de la ressource (identifier)
+  
+  return dataIdentifier
+  
+  if isfile(joinpath(path, "datasUploaded.csv"))
+    f = open(joinpath(path, "datasUploaded.csv"), "a")       
+      write(f, "\n"*directory*","*dataIdentifier)
+    close(f)      
+  else
+    touch(joinpath(path, "datasUploaded.csv"))
+    f = open(joinpath(path, "datasUploaded.csv"), "w") 
+      write(f, "ressource,identifiant")
+      write(f, "\n"*directory*","*dataIdentifier)
+    close(f)
+  end
+
+  
+  touch(joinpath(path, directory, directory*".csv"))
+  f = open(joinpath(path, directory, directory*".csv"), "w") 
+    write(f, "filename,identifier,fileIdentifier")
+    for file in filesInfo
+      write(f, "\n"*file[1]*","*dataIdentifier*","*file[2])
+    end
+  close(f)
+end
 
 end # end module
--- test/utilities.jl
+++ test/utilities.jl
@@ -1,6 +1,143 @@
 using Test
 using HTTP, JSON
 
-datas = Nakala.Search.search([:size=>"100"], true)
-datas["body"]["datas"][1]
-println(Nakala.Utilities.getdatas_resume(datas["body"]["datas"]))
+
+path = @__DIR__
+apikey = "01234567-89ab-cdef-0123-456789abcdef" # tnakala user public key for Nakala test api
+userid = "c7e9bb15-6b4e-4e09-b234-ae7b13ac1f3b" # unakala1 public user id
+
+#==
+    Obtenir des paires titre - identifiant à partir d'une liste de données
+==#
+datas = Nakala.Search.search([:q => "édition", :size=>"20"], true)
+Nakala.Utilities.getdatas_resume(datas["body"]["datas"])
+
+
+#==
+    Obtenir les urls de téléchargement d'une liste de fichiers dans une donnée
+==#
+# obtenir d'un donnée
+identifier = "10.34847/nkl.a5505848"
+headers = Dict(
+    "X-API-KEY" => apikey,
+    "Content-Type" => "application/json"
+)
+getdatas_response = Nakala.Datas.getdatas(identifier, headers, true)
+
+wanted_files = [ "guepes-TEI--trad-Lobineau--XVIIIe.xml" ]
+getfiles_urls_from_data_response = Nakala.Utilities.getfiles_urls_from_data(getdatas_response["body"], wanted_files, true)
+println(getfiles_urls_from_data_response)
+
+#==
+    Lister le contenu d'un dossier
+==#
+Nakala.Utilities.listfiles("/home/josselin/files/dh/Nakala.jl/test/testdata")
+
+for (root, dirs, files) in walkdir("/home/josselin/files/dh/Nakala.jl/test/testdata")
+    filter!(x -> startswith(x, ".") == false, dirs)
+    for dir in dirs
+      touch(joinpath(root, dir, "files.csv"))
+      f = open(joinpath(root, dir, "files.csv"), "w")
+        write(f, "filename")
+      close(f)
+
+      println(dir)
+      push!(directories, dir)
+
+      list = readdir(joinpath(root, dir))
+
+      for file in list
+        f = open(joinpath(root, dir, "files.csv"), "a")
+          write(f, "\n"*file)
+        close(f)
+      end
+    end
+  end
+
+
+  using CSV
+using DataFrames
+
+# Fonction pour lister les fichiers dans un dossier et écrire dans un fichier CSV
+function listfiles(dir::String)
+    # Liste pour stocker les chemins complets des fichiers
+    files = []
+
+    # Parcourir les entrées du dossier, sans entrer dans les sous-dossiers
+    for entry in readdir(dir, join=true)
+        if isfile(entry)  # Vérifie que c'est un fichier
+            push!(files, entry)
+        end
+    end
+
+    # Si aucun fichier n'est trouvé, on retourne un message d'erreur
+    if isempty(files)
+        println("Aucun fichier trouvé dans le dossier : $dir")
+        return
+    end
+
+    # Créer un DataFrame avec les chemins des fichiers
+    df = DataFrame(FilePath = files)
+
+    # Écrire dans le fichier CSV
+    CSV.write(joinpath(dir, "files.csv"), df)
+
+    println("Le fichier files.csv a été créé avec succès.")
+end
+
+apikey = "01234567-89ab-cdef-0123-456789abcdef" # tnakala user public key for Nakala test api
+
+
+function uploadfiles_from_csv(path::String)
+    df = CSV.read(path, DataFrame)
+    df.SHA1 = Vector{Union{String, Missing}}(undef, nrow(df))
+    for i in 1:nrow(df)
+        file_path = df.FilePath[i]
+        # Vérifier que le fichier existe
+        if isfile(file_path)
+            headers = Dict( "X-API-KEY" => apikey, :accept => "application/json" )
+            postdatas_uploads_response = Nakala.postdatas_uploads(file_path, headers, true)
+            sha1 = postdatas_uploads_response["body"]["sha1"]
+            df.SHA1[i] = sha1
+        else
+            println("Fichier introuvable : $file_path")
+            df.SHA1[i] = missing
+        end
+    end
+
+    # Sauvegarder le CSV avec la colonne SHA1
+    CSV.write("files_with_sha1.csv", df)
+    println("Le fichier files_with_sha1.csv a été mis à jour avec les codes SHA1.")
+    return df
+end
+
+uploadfiles_from_csv("/home/josselin/files/dh/Nakala.jl/test/testdata/files.csv")
+
+function process_files_and_add_sha1(csv_file::String, api_url::String)
+    # Lire le fichier CSV
+    
+
+    # Ajouter une nouvelle colonne pour les SHA1
+    
+
+    # Boucler sur chaque fichier et envoyer à l'API Nakala
+    for i in 1:nrow(df)
+        file_path = df.FilePath[i]
+
+        # Vérifier que le fichier existe
+        if isfile(file_path)
+            # Envoyer le fichier à l'API et récupérer le SHA1
+            sha1 = send_to_nakala(file_path, api_url)
+            df.SHA1[i] = sha1
+        else
+            println("Fichier introuvable : $file_path")
+            df.SHA1[i] = missing
+        end
+    end
+
+    # Sauvegarder le CSV avec la colonne SHA1
+    CSV.write("files_with_sha1.csv", df)
+    println("Le fichier files_with_sha1.csv a été mis à jour avec les codes SHA1.")
+end
+
+listfiles("/home/josselin/files/dh/Nakala.jl/test/testdata/")
