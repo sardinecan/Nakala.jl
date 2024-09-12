6370c06f56c6a801ee912374674a6ae68b2b8831
sardinecan <morvan.josselin@gmail.com>
2024-09-12 11:35:30 +0200

    added example for utilities module

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -1,5 +1,25 @@
 module Utilities
 
+"""
+  contains_key_value(d::Dict, key::Union{String, Symbol}, value::String)
+
+Vérifie qu'un dictionnaire (`d`) dispose d'une clé `key` dont la valeur est `value`. La casse de la clé est ignorée, de même que son type (`string` ou `symbol`).
+
+# Exemple
+```julia-repl
+julia> d = Dict(
+  :Accept => "application/json"
+)
+Dict{Symbol, String} with 1 entry:
+  :Accept => "application/json"
+
+julia> key, value = "accept", "application/json"
+("accept", "application/json")
+
+julia> contains_key_value(d, key, value)
+true
+```
+"""
 function contains_key_value(d::Dict, key::Union{String, Symbol}, value::String)
   key_lower = lowercase(string(key))
 
@@ -12,5 +32,13 @@ function contains_key_value(d::Dict, key::Union{String, Symbol}, value::String)
   return false
 end
 export contains_key_value
+
+d = Dict(
+  :Accept => "application/json"
+)
+
+key, value = "accept", "application/json"
+
+contains_key_value(d, key, value)
 
 end # end module
